7136dd75344e31543978afade368d11328d89aeb
William Ross <william@rillyboss.com>
2019-02-25 22:40:53 -0700

    Set heal request delay to 1 second. Upped default potion count to 200 and rebuy count to 20.

--- Config.js
+++ Config.js
@@ -1,9 +1,9 @@
 let interval = 1000 / 4;
-let potionThreshold = 10;
-let potionBuyCount = 100;
+let potionThreshold = 20;
+let potionBuyCount = 200;
 let state = "";
 let enemyToKill = ENEMY_TYPES.GOO;
 let nextValidHealRequestDate = new Date();
-let healRequestDelayInSeconds = 5;
+let healRequestDelayInSeconds = 1;
 let sayIntervalInSeconds = 15;
 let nextSayAllowedDate = new Date();
