04029d6a6198d8358c9f0f285626fc0323664592
William Ross <william@rillyboss.com>
2019-02-25 20:11:48 -0700

    Fixed casing on forEach. God Damned JS.

--- CodeRepository.js
+++ CodeRepository.js
@@ -27,7 +27,7 @@ getCodeFromRemoteBranch = (branch) => {
     parent.api_call("list_codes", {
         callback: function () {
             game_log("Updating from remote branch " + branch);
-            CodeFiles.foreach(file => {
+            CodeFiles.forEach(file => {
                 let request = new XMLHttpRequest();
                 let path = repoURL.replace(":branch", branch) + file.name + file.extension;
                 request.open("GET", path);
